dfe3bdca1a97cdd8685da28ecbf77122ca024182
user <user@DESKTOP-4SOQU3K>
2017-10-11 15:30:32 +0900

    .

--- WebClass/WebContent/jsp/home2.jsp
+++ WebClass/WebContent/jsp/home2.jsp
@@ -1,19 +1,20 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
 	pageEncoding="UTF-8"%>
-<%@ page import="org.dimigo.vo.UserVO"%>
-
 <!DOCTYPE html>
 <html lang="ko">
 <head>
+<!-- Required meta tags -->
 <meta charset="utf-8">
 <meta name="viewport"
 	content="width=device-width, initial-scale=1, shrink-to-fit=no">
-<title>Home</title>
+
+<!-- Bootstrap CSS -->
 <link rel="stylesheet"
 	href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css"
 	integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M"
 	crossorigin="anonymous">
-<link rel="stylesheet" href="/WebClass/css/footer.css">
+<link rel="stylesheet" href="../css/footer.css">
+
 <style>
 div.container {
 	padding-top: 30px;
@@ -23,17 +24,17 @@ div.container {
 
 <script>
 	function menu_over(e) {
-		e.setAttribute("class", "nav-item active");
+		e.setAttribute("class", "nav-item active"); //<li class="nav-item active">
 	}
 	function menu_out(e) {
-		e.setAttribute("class", "nav-item");
+		e.setAttribute("class", "nav-item"); //<li class="nav-item">
 	}
 </script>
+
 </head>
 <body>
-
 	<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-		<a class="navbar-brand" href="#">Home</a>
+		<a class="navbar-brand" href="#">Navbar</a>
 		<button class="navbar-toggler" type="button" data-toggle="collapse"
 			data-target="#navbarSupportedContent"
 			aria-controls="navbarSupportedContent" aria-expanded="false"
@@ -43,44 +44,19 @@ div.container {
 
 		<div class="collapse navbar-collapse" id="navbarSupportedContent">
 			<%@ include file="menu.jsp"%>
-
-			<%-- 세션이 없는 경우 --%>
-			<%
-				UserVO user = (UserVO) session.getAttribute("user");
-				if (user == null) {
-			%>
-			<a class="text-bold text-white" style="text-decoration: none"
-				href="/WebClass/login" method="post">Sign in</a> <span
-				class="text-bold text-white">&nbsp; or &nbsp;</span> <a
-				class="text-bold text-white" style="text-decoration: none" href="/WebClass/signup" method = "get">Sign
-				up</a>
-			<%
-				} else {
-			%>
-			<%-- 세션이 있는 경우 --%>
-			<ul class="navbar-nav flex-row ml-md-auto d-none d-md-flex">
-				<li class="nav-item dropdown"><a
-					class="nav-item nav-link dropdown-toggle mr-md-2" href="#"
-					id="bd-versions" data-toggle="dropdown" aria-haspopup="true"
-					aria-expanded="false"> <%=user.getName()%>님
-				</a>
-					<div class="dropdown-menu dropdown-menu-right"
-						aria-labelledby="bd-versions">
-						<form action="/WebClass/logout" method="post">
-							<button type="submit" class="dropdown-item"1>Sign out</button>
-						</form>
-						<div class="dropdown-divider"></div>
-						<button type="button" class="dropdown-item">Action1</button>
-						<button type="button" class="dropdown-item">Action2</button>
-					</div></li>
-			</ul>
-			<%
-				}
-			%>
+			<form class="form-inline my-2 my-lg-0" id="LoginForm">
+				<input class="form-control mr-sm-2" type="text" placeholder="ID"
+					aria-label="ID" ID="id" required> <input
+					class="form-control mr-sm-2" type="text" placeholder="PWD"
+					aria-label="PWD" Id="pwd" required>
+				<button class="btn btn-outline-success my-2 my-sm-0" type="submit">Login</button>
+			</form>
 		</div>
 	</nav>
+	
+
 	<div class="container">
-		<h1>Hello, Bootstrap</h1>
+		<h1>Hello, world!</h1>
 		<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
 			Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque
 			penatibus et magnis dis parturient montes, nascetur ridiculus mus.
@@ -103,19 +79,11 @@ div.container {
 			nibh. Donec sodales sagittis magna. Sed consequat, leo eget bibendum
 			sodales, augue velit cursus nunc,</p>
 	</div>
-
+	<!-- 모달 -->
+	
+	<%@ include file="modal.jsp"%>
 	<%@ include file="footer.jsp"%>
 
-	<!-- Bootstrap js -->
-	<script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
-	<script
-		src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js"
-		integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4"
-		crossorigin="anonymous"></script>
-	<script
-		src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js"
-		integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1"
-		crossorigin="anonymous"></script>
 
 </body>
 </html>
--- WebClass/src/org/dimigo/servlet/LoginServlet.java
+++ WebClass/src/org/dimigo/servlet/LoginServlet.java
@@ -3,13 +3,16 @@ package org.dimigo.servlet;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.SessionCookieConfig;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
-import org.json.simple.JSONArray;
+import org.dimigo.vo.UserVO;
 import org.json.simple.JSONObject;
 
 import com.google.gson.Gson;
@@ -19,61 +22,82 @@ import com.google.gson.JsonObject;
 /**
  * Servlet implementation class LoginServlet
  */
-@WebServlet({"/login" })
+@WebServlet("/login")
+
 public class LoginServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public LoginServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+   private static final long serialVersionUID = 1L;
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
+   /**
+    * @see HttpServlet#HttpServlet()
+    */
+   public LoginServlet() {
+      super();
+      // TODO Auto-generated constructor stub
+   }
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	@SuppressWarnings("unchecked")
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.setContentType("application/json; charset=utf-8");
-		PrintWriter out = response.getWriter();
-		
-		request.setCharacterEncoding("utf-8");
-		String id = request.getParameter("id");
-		String pwd = request.getParameter("pwd");
-		System.out.printf("id : %s, pwd : %s\n",id ,pwd);
-		/*
-		 * {
-		 * 	"id" : test
-		 * }
-		 * */
-//		out.println("{");
-//		out.println("\"id\":"+"\""+id+"\"");
-//		out.println("}");
-		
-		// JSON simple library 사용
-//		JSONObject json = new JSONObject();
-//		json.put("id", id);
-//		System.out.println(json.toJSONString());
-//		out.write(json.toJSONString());
+   /**
+    * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+    *      response)
+    */
+   protected void doGet(HttpServletRequest request, HttpServletResponse response)
+         throws ServletException, IOException {
+      RequestDispatcher rd = request.getRequestDispatcher("/jsp/login.jsp");
+      rd.forward(request, response);
+   }
 
-		// Gson library 사용
-		Gson gson = new Gson();
-	      JsonObject json = new JsonObject();
-	      json.addProperty("id",id);
-	      String j = gson.toJson(json);
-	      System.out.println(j);
-	      out.write(j);
-	      out.close();
-	}
+   /**
+    * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+    *      response)
+    */
+   protected void doPost(HttpServletRequest request, HttpServletResponse response)
+         throws ServletException, IOException {
+      response.setContentType("text/html;charset=utf-8");
+      PrintWriter out = response.getWriter();
+
+      request.setCharacterEncoding("utf-8");
+      String id = request.getParameter("id");
+      String pwd = request.getParameter("pwd");
+      System.out.printf("id : %s, pwd : %s", id, pwd);
+
+      boolean result = true;
+      if (result) {
+         // 세션에 사용자 생성
+         HttpSession session = request.getSession();
+         UserVO user = new UserVO();
+         user.setId(id);
+         user.setName("홍길동");
+         user.setNickname("의적");
+
+         session.setAttribute("user", user);
+         
+         RequestDispatcher rd = request.getRequestDispatcher("jsp/home.jsp");
+         rd.forward(request, response);
+      }else {
+         request.setAttribute("msg", "error");
+         RequestDispatcher rd = request.getRequestDispatcher("jsp/login.jsp");
+         rd.forward(request, response);
+      }
+      out.close();
+   }
+
+   protected void doPost2(HttpServletRequest request, HttpServletResponse response)
+         throws ServletException, IOException {
+      // TODO Auto-generated method stub
+      response.setContentType("application/json;charset=utf-8");
+      PrintWriter out = response.getWriter();
+
+      String id = request.getParameter("id");
+      String pwd = request.getParameter("pwd");
+
+      JsonObject jo = new JsonObject();
+
+      jo.addProperty("id", id);
+
+      out.write(jo.toString());
+
+      System.out.println(jo.toString());
+
+      out.close();
+   }
 
 }
